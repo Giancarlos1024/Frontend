393e42a54195b55e04d8b563bd37e6069166476d
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2024-11-07 23:49:19 -0500

    se agrego el limite de advertencia

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -8,22 +7,23 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000', // Cambia a `process.env` aquí
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
+  build: {
+    // Ajuste el límite de tamaño de fragmentos
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de advertencia a 1000 KB
+
+    rollupOptions: {
+      output: {
+        // Manualmente divide fragmentos grandes
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'chart.js'], // Ejemplo con librerías comunes
+        },
+      },
+    },
+  },
 });
-
-
-//LOCALHOST
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
