df0a044ea5d09817b6518f9bbfcd955f0b7c482c
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2024-11-08 00:07:29 -0500

    se agrego el host

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Asegúrate de que se escuche en todas las interfaces
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:3000',
