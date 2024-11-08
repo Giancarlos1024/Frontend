7f1e1f4d6595c2946c9c4b6b0f1f490394157f7f
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2024-11-08 00:42:40 -0500

    se mejoro el proyecto del frontend

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), // Asegura que NODE_ENV esté definido correctamente
-  },
   server: {
-    host: '0.0.0.0', // Permite que Render detecte y exponga el servicio correctamente
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3000', // Cambia a `process.env` aquí
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'chart.js'],
-        },
-      },
-    },
-  },
 });
+
+
+//LOCALHOST
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
